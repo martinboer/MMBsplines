2e0ec1a1a1bc0fb740f25bcad08162786b80a474
Martin Boer <martin.boer@wur.nl>
2015-02-10 19:35:32 +0100

    several updates example.r

--- examples/example.r
+++ examples/example.r
@@ -1,36 +1,40 @@
 #
-# Martin Boer
-#
+# Martin Boer, 
+# Biometris WUR, Wageningen, The Netherlands.
+# 
 rm(list = ls())
 library('MMBsplines')
 
-# Set parameters:
+# set parameters:
 nobs = 1000
 xmin = 0
 xmax = 10
-nseg = 100
 
-# Simulate data:
+# simulate data:
 set.seed(949030)
 sim.fun = function(x) { return(3.0 + 0.1*x + sin(2*pi*x))}
 x = runif(nobs, min = xmin, max = xmax)
 y = sim.fun(x) + 0.5*rnorm(nobs)
 
-# run MMBsplines, using quadratic B-splines (default)
-obj = MMBsplines(x, y, xmin, xmax, nseg = nseg)
+# calculate lambda_max:
+obj = MMBsplines(x, y, xmin, xmax, nseg = 100)
 
-# make predictions on a small grid: 
+# predictions on a dense grid:
 x0 = seq(xmin, xmax, by=0.01)
 yhat = predict(obj, x0)
 ylin = predict(obj, x0, linear=TRUE)
 ysim = sim.fun(x0)
-
-# print summary:
 summary(obj)
 
 # plot:
-plot(x=x, y=y, col='black', pch=16, cex=0.5)
+plot(x=x, y=y, col='black', pch=16, cex=0.5,ylim=c(0,8))
 lines(x = x0, y = ysim, col='blue', lwd=2.5)
 lines(x = x0, y = yhat, col='red', lwd=2.5)
 lines(x = x0, y = ylin, col='green', lwd=2.5)
+legend(0.3,8,c("y","ysim","yhat","ylin"), # puts text in the legend
+  pch=c(16,NA,NA,NA),
+  lty=c(NA,1,1,1), # gives the legend appropriate symbols (lines)
+  lwd=c(NA,2.5,2.5,2.5),
+  col=c("black","blue","red","green")) 
+
 
